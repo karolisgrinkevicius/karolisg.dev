e10ca1ab82da58a21c675038fb1f2c3ca2043295
Karolis Grinkevičius <karolis.ext.grinkevicius@swedbank.lt>
2024-12-09 15:16:07 +0200

    Refactor avatar image

--- src/lib/Avatar.svelte
+++ src/lib/Avatar.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
   import { Image } from '@unpic/svelte';
 
-  const CLOUDINARY_ENV = 'dzm0tz8vx';
-  const SRC_PREFIX = `https://res.cloudinary.com/${CLOUDINARY_ENV}/image/upload/`;
-  const SRC_VERSION = 'v1731608929';
-  const SRC_ID = 'bc17e3f2091ff48ff4b3d8e3433e947f_ndgrxh';
-  const EXT = 'PNG';
+  const getImageSrc = () => {
+    const CLOUDINARY_ENV = 'dzm0tz8vx';
+    const SRC_PREFIX = `https://res.cloudinary.com/${CLOUDINARY_ENV}/image/upload/`;
+    const SRC_VERSION = 'v1731608929';
+    const SRC_ID = 'bc17e3f2091ff48ff4b3d8e3433e947f_ndgrxh';
+    const EXT = 'PNG';
+
+    return `${SRC_PREFIX}${SRC_VERSION}/${SRC_ID}.${EXT}`;
+  };
+
+  const imageSrc = getImageSrc();
 </script>
 
 <Image
-  src={`${SRC_PREFIX}${SRC_VERSION}/${SRC_ID}.${EXT}`}
+  src={imageSrc}
   alt="Karolis Grinkevičius"
   fetchpriority="high"
   loading="eager"
